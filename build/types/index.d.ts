eb0efc7332ebd2d74cab8d500c557b1739152efa
Wijnand Karsens <wijnand@karsens.com>
2024-04-23 10:17:11 +0200

    improvements

--- build/types/index.d.ts
+++ build/types/index.d.ts
@@ -4,4 +4,6 @@ export * from "./openapi.schema.js";
 export * from "./schema-list.schema.js";
 export * from "./local-storage.schema.js";
 export * from "./types.js";
+import type { OpenapiProxySchema } from "./openapi-proxy.schema.js";
+export type { OpenapiProxySchema };
 //# sourceMappingURL=index.d.ts.map
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,5 @@ export * from "./openapi.schema.js";
 export * from "./schema-list.schema.js";
 export * from "./local-storage.schema.js";
 export * from "./types.js";
+import type { OpenapiProxySchema } from "./openapi-proxy.schema.js";
+export type { OpenapiProxySchema };
